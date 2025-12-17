520c53e7a9223cf5ad1636f3cd79f21ff472aab0
Prateet Tiwari <prateettiwari29@gmail.com>
2025-12-17 17:32:28 +0530

    feat: add CORS middleware and enhance health check route with environment info and uptime

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import healthRoute from './routes/health.route.js';
+import cors from 'cors';
 
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use('/api', healthRoute);
+
+// 404 Handler
+app.use((_req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 export default app;
--- routes/health.route.js
+++ routes/health.route.js
@@ -1,9 +1,14 @@
 import {Router} from 'express';
+import env from '../config/env.js';
 
 const router = Router();
 
 router.get('/health', (_req, res) => {
-  res.status(200).json({status: 'OK', service: 'authly'});
+  res.status(200).json({
+    status: 'OK', 
+    service: 'authly', 
+    environment: env.NODE_ENV, 
+    uptime: process.uptime()});
 });
 
 export default router;
